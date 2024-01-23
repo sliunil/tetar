c0f82f9c6063850504c741f51c4d5b2a55f20ca0
gguex <guillaume.guex@gmail.com>
2024-01-23 13:13:02 +0100

    review the parsing of legal xml file

--- data/real_corpora/raw/legal/parse_xml_law.py
+++ data/real_corpora/raw/legal/parse_xml_law.py
@@ -1,4 +1,5 @@
 from xml.dom.minidom import parse 
+import re
 import os
 
 # The name of the input and output directories
@@ -12,7 +13,13 @@ def parse_text(nodelist):
         if node.nodeType == node.TEXT_NODE:
             final_text.append(node.data)
         elif len(node.childNodes) > 0:
-            inside_text = parse_text(node.childNodes)
+            restricted_childNodes = []
+            for childNode in node.childNodes:
+                if childNode.nodeType != node.TEXT_NODE:
+                    if childNode.tagName in ('note', 'sourceCredit'):
+                        continue
+                restricted_childNodes.append(childNode)
+            inside_text = parse_text(restricted_childNodes)
             final_text.extend(inside_text)
     return ''.join(final_text)
 
@@ -21,13 +28,14 @@ file_names = os.listdir(input_directory_name)
 
 # Loop on files
 for file_name in file_names:
-
     # Open the file
     with open(f"{input_directory_name}/{file_name}") as file:
         document = parse(file)
 
     # Get the textual data
-    parsed_text = parse_text(document.childNodes)
+    parsed_text = parse_text(document.getElementsByTagName("section"))
+    # Remove extra "end of line"
+    parsed_text = re.sub("\n{2,}", "\n\n", parsed_text)
 
     # Write the resulting file
     with open(f"{output_directory_name}/{file_name[:-4]}.txt", "w") as file:
