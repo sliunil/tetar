171aa19c45b457f822ad64b5051d87b9d1de1c78
gguex <guillaume.guex@gmail.com>
2024-01-24 10:03:04 +0100

    compute real corpora stats

--- src/real_corpora_ld_stats.py
+++ src/real_corpora_ld_stats.py
@@ -1,15 +1,41 @@
 import pandas as pd
 import numpy as np
 
+
+
+# -------------------------------
+# --- SCRIPT PARAMETERS
+# -------------------------------
+
 input_file_path = "../results/real_corpora_indices_5000_10.csv"
-output_file_path = "../results/correlation_matrix_new.xlsx"
+output_cor_path = "../results/real_corpora_correlation.xlsx"
+output_mean_path = "../results/real_corpora_group_mean.csv"
 
-data = pd.read_csv(input_file_path, index_col=0)
 
-num_data = data.drop(["name", "group"], axis=1)
 
-correlations = np.corrcoef(num_data.to_numpy().T)
+# -------------------------------
+# --- CODE
+# -------------------------------
 
-correlations_df = pd.DataFrame(correlations, columns=num_data.columns, index=num_data.columns)
+# ---- Correlations
 
-correlations_df.style.background_gradient(cmap="Reds").to_excel(output_file_path)
+# Load dataset
+ld_stat_df = pd.read_csv(input_file_path, index_col=0)
+
+# Keep numerical data
+num_data = ld_stat_df.drop(["name", "group"], axis=1)
+
+# Compute correlation and save them with color
+correlations_df = pd.DataFrame(np.corrcoef(num_data.to_numpy().T), 
+                               columns=num_data.columns, 
+                               index=num_data.columns)
+correlations_df.style.background_gradient(cmap="Reds").to_excel(output_cor_path)
+
+# ---- Groups means
+
+# Compute de mean by group
+ld_stat_df_gpd = ld_stat_df.drop("name", axis=1).groupby("group")
+ld_mean_df = ld_stat_df_gpd.mean()
+
+# Save it
+ld_mean_df.to_csv(output_mean_path)
