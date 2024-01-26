bb64626d7ef28606cb462cb90aeef9edc9f1c96f
gguex <guillaume.guex@gmail.com>
2024-01-26 17:10:23 +0100

    update code for results

--- src/sample_size_analyse_res.py
+++ src/sample_size_analyse_res.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 
 # -------------------------------
@@ -9,9 +10,7 @@ import numpy as np
 
 input_file_path = "../results/sample_size/sample_size_2000-29000_50-2000.csv"
 output_folder_path = "../results"
-measure_names = ["sample_entropy", "subsample_entropy_rdm", 
-                 "subsample_entropy_mav", "exp_variety", "mtld"]
-
+output_file_prefix = "sample_size_plot"
 
 
 # -------------------------------
@@ -21,8 +20,37 @@ measure_names = ["sample_entropy", "subsample_entropy_rdm",
 # Load dataset
 sample_size_df = pd.read_csv(input_file_path)
 
+# Get measure names 
+measure_names = sample_size_df.columns.to_numpy()[6:]
 
+# Get genres (color)
+genres = sample_size_df.genre.unique()
+color_map = cm.get_cmap("hsv", len(genres) + 1)
 
-# Color group
-sample_size_df.genre.unique()
+# Get subsample lengths (alpha)
+subsample_lens = sample_size_df.subsample_len.unique()
+alphas = np.linspace(0.5, 1, len(subsample_lens))
 
+# Get reduced sample size
+reduced_sample_sizes = sample_size_df.reduced_sample_size.unique()
+
+# for mesure names
+measure_name = measure_names[0]
+
+# for genres
+genre = genres[0]
+genre_id = 0
+
+# for subsample lengths
+subsample_len = subsample_lens[0]
+subsample_len_id = 0
+
+# Restricted dataframe
+sample_size_df[(sample_size_df.genre == genre) &
+               (sample_size_df.subsample_len == subsample_len)]
+
+# Groupby mean and sd
+grpd_sample_size_df = sample_size_df.groupby(["reduced_sample_size"])
+measure_mean = grpd_sample_size_df[measure_name].mean()
+measure_std = grpd_sample_size_df[measure_name].std()
+n_tests = int(len(sample_size_df) / len(measure_mean))
