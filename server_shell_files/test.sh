9802f0c1cd15ea7014ed9550af4c9ca7a5e3b98a
gguex <guillaume.guex@gmail.com>
2024-01-26 15:39:50 +0100

    compute results

--- server_shell_files/test.sh
+++ server_shell_files/test.sh
@@ -12,13 +12,13 @@
 
 #SBATCH --nodes 1 
 #SBATCH --ntasks 1 
-#SBATCH --cpus-per-task 8
-#SBATCH --mem 64G 
-#SBATCH --time 24:00:00 
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 32G 
+#SBATCH --time 48:00:00 
 #SBATCH --export NONE
 
 module load gcc python
 
-source ~/tetar/.env/bin/activate
+source ~/tetar/.venv/bin/activate
 
 python3 sample_size_compute_res.py
