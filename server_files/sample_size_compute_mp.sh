7475956f985e2293b72d498c5c9c3231ba45761d
gguex <guillaume.guex@gmail.com>
2024-01-29 14:10:12 +0100

    update multiproc batch file

--- server_files/sample_size_compute_mp.sh
+++ server_files/sample_size_compute_mp.sh
@@ -6,14 +6,14 @@
 
 #SBATCH --chdir ../src/
 #SBATCH --job-name sample_size_compute_mp
-#SBATCH --output ../server_files/sample_size_compute_mp.out
+#SBATCH --output ../server_files/out_files/sample_size_compute_mp_%A_%a.out
 
 #SBATCH --partition cpu
 #SBATCH --ntasks 1 
 
 #SBATCH --cpus-per-task 12
 #SBATCH --mem 32G 
-#SBATCH --time 48:00:00 
+#SBATCH --time 12:00:00 
 #SBATCH --export NONE
 
 #SBATCH --array=0-29
@@ -22,4 +22,4 @@ module load gcc python
 
 source ~/tetar/.venv/bin/activate
 
-python3 sample_size_compute_res.py $SLURM_ARRAY_TASK_ID
+python3 sample_size_compute_res_server.py $SLURM_ARRAY_TASK_ID
