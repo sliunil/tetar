ae89e07db02c51caf438fdaecef2ffc2951c8b54
gguex <guillaume.guex@gmail.com>
2024-02-04 14:06:33 +0100

    merge ac results

---
+++ results/artificial_corpora/merge_ac_results.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+# -------------------------------
+# --- SCRIPT PARAMETERS
+# -------------------------------
+
+input_folder_path = "ac_results_1/all_files"
+output_file_path = "ac_results_1/ac_50-1001_10-500.csv"
+
+# -------------------------------
+# --- CODE
+# -------------------------------
+
+files = os.listdir(input_folder_path)
+for i, file in enumerate(files):
+    if i == 0:
+        results_df = pd.read_csv(f"{input_folder_path}/{file}")
+    else:
+        new_file_df = pd.read_csv(f"{input_folder_path}/{file}")
+        results_df = pd.concat([results_df, new_file_df])
+               
+results_df.to_csv(output_file_path, index=None)
