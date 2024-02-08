f4ea38b39d412a3e0378da742f06e194b1e22b27
gguex <guillaume.guex@gmail.com>
2024-02-08 10:09:01 +0100

    plot results for zm 1

--- src/artificial_corpora_analyse_res.py
+++ src/artificial_corpora_analyse_res.py
@@ -8,9 +8,9 @@ from matplotlib import cm
 # --- SCRIPT PARAMETERS
 # -------------------------------
 
-input_file_path = "../results/artificial_corpora/acz_results_3/acz_3_merge.csv"
+input_file_path = "../results/artificial_corpora/zm_res_1/zm_1_merge.csv"
 output_folder_path = "../results"
-output_file_prefix = "acz_plot"
+output_file_prefix = "zm_plot"
 min_prop_to_compute_mtld = 0.6
 
 
@@ -21,60 +21,51 @@ min_prop_to_compute_mtld = 0.6
 # Load dataset
 ac_df = pd.read_csv(input_file_path)
 
-# Get measure names 
-measure_names = ac_df.columns.to_numpy()[4:]
-measure_clean_names = ["Sample Entropy", "Subsample Entropy (rdm)", 
-                       "Subsample Entropy (mav)", "HD-D", 
-                       "MTLD"]
-
-for measure_name_id, measure_name in enumerate(measure_names):
-
-    # Get subsample lengths (linewidth)
-    if not measure_name == 'mtld':
-        subsample_lens = ac_df.subsample_len.unique()
-    else:
-        subsample_lens = ac_df.mtld_thresholds.unique()
-    linestyles = [(0, (3*i+1, 0.5*i+1)) for i, _ in enumerate(subsample_lens)]
-    linestyles[-1] = 'solid'
+# Get measure names and define drawing 
+measure_names = ac_df.columns.to_numpy()[5:]
+measure_names = measure_names[np.array([0, 1, 3, 4])]
+measure_clean_names = ["Sample Entropy", "Subsample Entropy (both)", 
+                       "HD-D", "MTLD"]
+color_map = cm.get_cmap("binary", len(measure_names) + 1)
+markers = ['o', 'v', 's', 'd']
     
+# Get the graph_type
+graph_types = ac_df["type"].unique()
+graph_type_names = ["Variety", "Power", "Shift"]
+
+# Normalization
+for measure_name in measure_names:
+    min_measure = ac_df[measure_name].min()
+    max_measure = ac_df[measure_name].max()
+    ac_df.loc[:, measure_name] = ac_df[measure_name] - min_measure
+    ac_df.loc[:, measure_name] = ac_df[measure_name] \
+        / (max_measure - min_measure)
+
+for id_type, graph_type in enumerate(graph_types):
+
+    ac_type_df = ac_df[ac_df.type == graph_type]
+
     plt.figure()
 
-    # for subsample lengths
-    for subsample_len_id, subsample_len in enumerate(subsample_lens):
-
-        # Selected dataframe
-        if not measure_name == 'mtld':
-            selected_df = ac_df[ac_df.subsample_len == subsample_len]
-        else: 
-            selected_df = ac_df[ac_df.mtld_thresholds == subsample_len]
-
+    for measure_name_id, measure_name in enumerate(measure_names):
+        
         # Groupby mean and sd
-        grpd_selected_df = selected_df.groupby(["slope"])
+        grpd_selected_df = ac_type_df.groupby([graph_type])
         measure_mean = grpd_selected_df[measure_name].mean()
         measure_std = grpd_selected_df[measure_name].std()
-        n_theo_tests = int(len(selected_df) / len(measure_mean))
+        n_theo_tests = int(len(ac_type_df) / len(measure_mean))
         n_tests = grpd_selected_df[measure_name].count()
         measure_mean.loc[n_tests / n_theo_tests 
                             < min_prop_to_compute_mtld] = np.nan
 
         # Plot it 
-        if not measure_name == 'sample_entropy':
-            plt.plot(measure_mean.index, measure_mean.values,
-                    color="black",
-                    linestyle=linestyles[subsample_len_id],
-                    label=subsample_len)
-        else:
-            plt.plot(measure_mean.index, measure_mean.values,
-                    color="black",
-                    linestyle=linestyles[-1])
+        plt.plot(measure_mean.index, measure_mean.values, 
+                color=color_map(measure_name_id+1),
+                marker=markers[measure_name_id], 
+                label=measure_clean_names[measure_name_id])
             
-        # plt.errorbar(measure_mean.index, measure_mean.values, 
-        #             yerr=measure_std.values*1.96/np.sqrt(n_theo_tests), 
-        #             color="black",
-        #             linestyle=linestyles[subsample_len_id])
-    if not measure_name == 'sample_entropy':
-        plt.legend(loc='upper right', fontsize='8')
-    plt.xlabel("Slope")
-    plt.ylabel(measure_clean_names[measure_name_id])
-    plt.savefig(f"{output_folder_path}/{output_file_prefix}_{measure_name}.png", 
-                dpi=1200)
+    plt.legend(loc='lower right', fontsize='8')
+    plt.xlabel(graph_type_names[id_type])
+    plt.ylim((0, 1))
+    plt.savefig(f"{output_folder_path}/{output_file_prefix}_{graph_type}.png", 
+                dpi=300)
--- src/old_scripts/artificial_corpora_analyse_res_old.py
+++ src/old_scripts/artificial_corpora_analyse_res_old.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import t
+from matplotlib import cm
+
 
 # -------------------------------
 # --- SCRIPT PARAMETERS
 # -------------------------------
 
-input_file_path = "../results/artificial_corpora/zm_res_1/zm_1_merge.csv"
+input_file_path = "../results/artificial_corpora/acz_results_3/acz_3_merge.csv"
 output_folder_path = "../results"
-output_file_prefix = "zm_plot"
+output_file_prefix = "acz_plot"
 min_prop_to_compute_mtld = 0.6
-shift_value = -1
-rolling_value = 20
 
 
 # -------------------------------
@@ -57,27 +56,15 @@ for measure_name_id, measure_name in enumerate(measure_names):
         n_tests = grpd_selected_df[measure_name].count()
         measure_mean.loc[n_tests / n_theo_tests 
                             < min_prop_to_compute_mtld] = np.nan
-        
-        # Pvalue computation
-        mx = measure_mean
-        mx_s = measure_mean.shift(shift_value)
-        nx = n_tests
-        nx_s = n_tests.shift(shift_value)
-        sx = measure_std
-        sx_s = measure_std.shift(shift_value)
-        val = np.abs(mx - mx_s) * np.sqrt(nx + nx_s - 2) \
-            / np.sqrt((1/nx + 1/nx_s)*(nx*sx**2 + nx_s*sx_s**2))
-        val = val.rolling(rolling_value).mean()
-        pval = 2*(1 - t.cdf(val, df=(nx + nx_s - 2)))
-        
+
         # Plot it 
         if not measure_name == 'sample_entropy':
-            plt.plot(measure_mean.index, pval,
+            plt.plot(measure_mean.index, measure_mean.values,
                     color="black",
                     linestyle=linestyles[subsample_len_id],
                     label=subsample_len)
         else:
-            plt.plot(measure_mean.index, pval,
+            plt.plot(measure_mean.index, measure_mean.values,
                     color="black",
                     linestyle=linestyles[-1])
             
@@ -85,14 +72,9 @@ for measure_name_id, measure_name in enumerate(measure_names):
         #             yerr=measure_std.values*1.96/np.sqrt(n_theo_tests), 
         #             color="black",
         #             linestyle=linestyles[subsample_len_id])
-        
-    
     if not measure_name == 'sample_entropy':
         plt.legend(loc='upper right', fontsize='8')
     plt.xlabel("Slope")
-    plt.ylabel(f"p-value for {measure_clean_names[measure_name_id]}")
-    plt.yscale("log")
-    plt.axhline(y=0.01, color="black")
-    plt.ylim([0, 0.1])
+    plt.ylabel(measure_clean_names[measure_name_id])
     plt.savefig(f"{output_folder_path}/{output_file_prefix}_{measure_name}.png", 
                 dpi=1200)
